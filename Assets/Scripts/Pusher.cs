f4f14981c8f8f2bebc610ffe85687edf28630be8
TsunenoriMotooka <tsune.motooka@gmail.com>
2025-04-11 13:12:13 +0900

    キャンディの実装

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -6,13 +6,16 @@ public class Pusher : MonoBehaviour
 {
     Vector3 startPosition;
 
-    public float amplitude;
-    public float speed;
+    [SerializeField]
+    float amplitude;
+    [SerializeField]
+    float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.localPosition;
+
     }
 
     // Update is called once per frame
@@ -22,3 +25,100 @@ public class Pusher : MonoBehaviour
         transform.localPosition = startPosition + new Vector3(0, 0, z);        
     }
 }
+
+/**
+@JAVA
+class Hero{
+    private String name;
+    private int hp;
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getHp(){
+        return this.hp;
+    }
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+}
+
+hero.setHp(hero.getHp() + 10)
+
+@C#
+class Hero{
+    string name;
+    int hp;
+
+    public string Name
+    {
+        get
+        {
+            return this.name
+        }        
+        set
+        {
+            this.name = value;
+        }
+    }
+    public int Hp
+    {
+        get
+        {
+            return this.hp;   
+        }
+        set
+        {
+            this.hp = hp;
+        }
+    }
+}
+
+hero.Hp += 10
+
+
+class Hero2{
+    public string Name{get;set;}
+    public int hp(get;set;)
+}
+
+
+ex)Transform
+
+@JAVA
+class Transform{
+    private Vector3 position;
+    
+    public Vector3 getPosition(){
+        return this.position;
+    }
+    public void setPosition(Vector3 position){
+        this.position = position   
+    }
+}
+
+@C#
+class Transform{
+    Vector3 _position;
+    
+    public Vector3 position
+    {
+        get
+        {
+            return this._position;
+        }
+        set
+        {
+            this._position = value;
+        }
+    }
+}
+
+class Tranform{
+    public Vector3 position{get;set;}
+}
+
+**/
